c6790e4237c3baf383c1d4bfc284b06e25ca6458
helow <helow@helow19274.tk>
2022-03-26 12:49:41 +0300

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='aiogTTS',
-    version='1.1.0',
+    version='1.1.1',
 
     author='Helow19274',
     author_email='helow@helow19274.tk',
